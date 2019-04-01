5756f215220394daae15e01fb4e8067348453371
Albert Rondon <hellomyfrend11@hotmail.com>
2019-04-01 22:12:06 +0000

    almost done

--- js/script.js
+++ js/script.js
@@ -2,9 +2,20 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-
 $("#search-button").click(function(){
-  
+    var userInput = $("input").val();
+ var request_url = "https://api.giphy.com/v1/gifs/search?q=" + userInput + "&rating=pg&api_key=dc6zaTOxFJmzC";
+ $.ajax({
+  url: request_url,
+  method: "GET",
+  success: function(response) {
+    $(".text-center").html("<img src='"+ response.data[0].images.fixed_height_still.url +"'/>");
+    
+
+    console.log(response);
+  },
+});
+
   
   
 });
